14cdd466dd3a3e571f39515bdfc563a9515ca58a
Mohamed Bouchkouj <bouchkoujmohamed8@gmail.com>
2024-10-07 21:37:07 +0100

    first commit - initailize the project-phones

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+let image=document.querySelector(".image");
+let continair=document.querySelector(".continair");
+function phones(srcv){
+    image.src=srcv
+}
+function colors(color){
+    continair.style.background=color
+}
